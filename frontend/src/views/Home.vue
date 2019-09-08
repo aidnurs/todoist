61edca9c1383a6226db90db545693f677e928515
Nursultan <nursultan_imanov@mail.ru>
2019-09-08 19:50:34 +0200

    [FrontEnd] getAll and post request methods

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,18 +1,72 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-  </div>
+    <div class="home">
+        <img alt="Vue logo" src="../assets/logo.png" />
+        <ul>
+            <li v-for="todo in this.todos">
+                <p>
+                    {{ todo.task }}
+                </p>
+                <!--<input type="checkbox" name="" value="" />-->
+            </li>
+        </ul>
+        <input type="text" name="" value="" v-model="todo.task" @keyup.enter="addTodo" />
+    </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import axios from 'axios';
 
 export default Vue.extend({
-  name: 'home',
-  components: {
-    HelloWorld,
-  },
+    name: 'home',
+    components: {},
+    data() {
+        return {
+            todo: {
+                task: '',
+                status: false,
+            },
+            todos: [] as Object[],
+        };
+    },
+    methods: {
+        addTodo() {
+            let test = JSON.stringify({
+                task: this.todo.task,
+                status: false,
+            });
+            console.log(typeof test);
+            axios
+                .post(
+                    process.env.VUE_APP_BACKEND + '/todo',
+                    JSON.stringify({
+                        task: this.todo.task,
+                        status: false,
+                    }),
+                    {
+                        headers: {
+                            'Content-Type': 'application/x-www-form-urlencoded',
+                        },
+                    },
+                )
+                .then(() => {
+                    this.getTodos();
+                });
+        },
+        getTodos() {
+            axios
+                .get(process.env.VUE_APP_BACKEND + '/todos')
+                .then((res) => {
+                    this.todos = res.data;
+                })
+                .catch((err) => {
+                    throw err;
+                });
+        },
+    },
+    mounted() {
+        this.getTodos();
+    },
 });
 </script>
