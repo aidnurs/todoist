4cdd93f3c49a6876358acc9371f1fde8581284f0
Nursultan <nursultan_imanov@mail.ru>
2019-09-18 21:31:20 +0200

    login route

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -3,8 +3,10 @@ const router = express.Router();
 const auth = require('../middleware/auth');
 const bcrypt = require('bcrypt');
 const { User, validate } = require('../models/user.model');
+const config = require('config');
+const jwt = require('jsonwebtoken');
 
-router.get('/', (req, res) => {
+router.get('/', auth, (req, res) => {
     User.find({})
         .populate('todos')
         .exec((err, data) => {
@@ -26,7 +28,7 @@ router.get('/current', auth, (req, res) => {
         });
 });
 
-router.post('/', (req, res) => {
+router.post('/signup', (req, res) => {
     // validate the request body first
     const { error } = validate(req.body);
     if (error) return res.status(400).send(error.details[0].message);
@@ -52,6 +54,35 @@ router.post('/', (req, res) => {
         res.header('x-auth-token', token).send({
             _id: user._id,
             username: user.username,
+        });
+    });
+});
+
+router.post('/login', (req, res) => {
+    User.findOne({ username: req.body.username }, (err, user) => {
+        if (err) {
+            return res.status(400).send(error.details[0].message);
+        }
+        bcrypt.compare(req.body.password, user.password, (err, isRightPassword) => {
+            if (err) {
+                return res.status(400).send(error.details[0].message);
+            }
+            if (!isRightPassword) {
+                res.status(403).json({
+                    success: false,
+                    message: 'Incorrect username or password',
+                });
+            } else {
+                const token = jwt.sign(
+                    { _id: user._id, isAdmin: user.isAdmin },
+                    config.get('myprivatekey'),
+                );
+                res.json({
+                    success: true,
+                    message: 'Authentication successful!',
+                    token: token,
+                });
+            }
         });
     });
 });
